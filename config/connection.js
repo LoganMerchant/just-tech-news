974d475323b77d29b326b038387fe248a2402ccd
Logan Merchant <merchantclogan@gmail.com>
2020-10-22 12:55:25 -0600

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,19 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // Create connection to our database
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
+let sequelize;
+
+// If going through Heroku
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} 
+// if going through my local machine
+else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+};
 
 module.exports = sequelize;
