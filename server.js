cc2755ebba2b089a2fb9f7cd103018b5626f6860
Logan Merchant <merchantclogan@gmail.com>
2020-10-26 21:38:07 -0600

    add express sessions

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const routes = require('./controllers');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 const sequelize = require('./config/connection');
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: process.env.cookie,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +24,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
